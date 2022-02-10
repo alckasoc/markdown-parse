698f58b6a6675c560f43a155da248a6fdbc77e67
Vincent Tu <tuvincent0106@gmail.com>
2022-02-09 19:13:02 -0800

    Update mdparse.sh

--- mdparse.sh
+++ mdparse.sh
@@ -1,1 +1,2 @@
-java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParse $1
+javac -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" MarkdownParse.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" MarkdownParse $1
