affb9231fad1480e2759be27354ec838cefcc1c8
Vincent Tu <tuvincent0106@gmail.com>
2022-02-22 22:28:50 -0800

    added 3 new snippet test cases

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -25,4 +25,25 @@ public class MarkdownParseTest {
         List<String> expect = List.of("https://something.com", "some-page.html");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
+
+    @Test
+    public void testSnippet1() throws IOException {
+        String contents= Files.readString(Path.of("./snippet1.md"));
+        List<String> expect = List.of("url.com", "google.com");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testSnippet2() throws IOException {
+        String contents= Files.readString(Path.of("./snippet2.md"));
+        List<String> expect = List.of("a.com", "b.com", "a.com", "a.com(())", "example.com");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testSnippet3() throws IOException {
+        String contents= Files.readString(Path.of("./snippet3.md"));
+        List<String> expect = List.of("https://www.twitter.com", "https://ucsd-cse15l-w22.github.io/", "github.com", "https://cse.ucsd.edu/");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
 }
